d61270f9b44db29355ab8a4e0a3dcc9396c13309
apexlions16 <hcywashere@gmail.com>
2026-08-07 19:12:46 +0300

    chore(studio): bump editorial and fast upload release to 1.5.0

--- apps/android-studio/app/build.gradle.kts
+++ apps/android-studio/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.studio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.4.1"
+        versionCode = 10
+        versionName = "1.5.0"
 
         vectorDrawables {
             useSupportLibrary = true
