e775e8a74234e4e6041fb029c262dbc40594ced5
apexlions16 <hcywashere@gmail.com>
2026-08-07 12:32:42 +0300

    chore(android-studio): direct medya surumunu 1.2.0 yap

--- apps/android-studio/app/build.gradle.kts
+++ apps/android-studio/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.studio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         vectorDrawables {
             useSupportLibrary = true
