5c08079ada9de0b0697bfa5e9b2e2006bc105f79
apexlions16 <hcywashere@gmail.com>
2026-08-07 17:03:11 +0300

    chore(studio): bump trailer-enabled build to 1.3.1

--- apps/android-studio/app/build.gradle.kts
+++ apps/android-studio/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.studio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.1"
 
         vectorDrawables {
             useSupportLibrary = true
