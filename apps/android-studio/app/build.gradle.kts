2a5e22dedc20df50948eb525a850962619dd1187
apexlions16 <hcywashere@gmail.com>
2026-08-07 11:54:12 +0300

    chore(android-studio): bump 1.1.1

--- apps/android-studio/app/build.gradle.kts
+++ apps/android-studio/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.studio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         vectorDrawables {
             useSupportLibrary = true
