a9f62dc3114df50c129cbefff36169d9b7b4b7d8
apexlions16 <hcywashere@gmail.com>
2026-08-07 16:17:23 +0300

    feat(studio): add quality generation route

--- apps/android-studio/app/src/main/kotlin/com/apexlions/film2/studio/navigation/Destinations.kt
+++ apps/android-studio/app/src/main/kotlin/com/apexlions/film2/studio/navigation/Destinations.kt
@@ -8,6 +8,9 @@ object Destinations {
     const val ATTACH_FILES_ROUTE = "attach-files/{titleId}/{titleType}"
     fun attachFiles(titleId: String, titleType: String) = "attach-files/$titleId/$titleType"
 
+    const val QUALITY_ROUTE = "quality/{titleId}/{titleType}"
+    fun quality(titleId: String, titleType: String) = "quality/$titleId/$titleType"
+
     const val ARG_TITLE_ID = "titleId"
     const val ARG_TITLE_TYPE = "titleType"
 }
