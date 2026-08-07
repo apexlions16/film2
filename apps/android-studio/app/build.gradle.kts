a1243498784426d7c5bae76f697f7c4eb50301ac
apexlions16 <hcywashere@gmail.com>
2026-08-07 17:32:52 +0300

    chore(studio): bump version to 1.3.2

--- apps/android-studio/app/build.gradle.kts
+++ apps/android-studio/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.studio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.3.2"
 
         vectorDrawables {
             useSupportLibrary = true
