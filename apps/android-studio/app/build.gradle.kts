b59155fcd03c0701162572fffd79ac4edff58a59
apexlions16 <hcywashere@gmail.com>
2026-08-07 18:08:36 +0300

    chore(studio): bump fast-publish build to 1.4.0

--- apps/android-studio/app/build.gradle.kts
+++ apps/android-studio/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.studio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.2"
+        versionCode = 8
+        versionName = "1.4.0"
 
         vectorDrawables {
             useSupportLibrary = true
