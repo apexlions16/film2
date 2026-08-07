e5474cc09972382741071d767700773d106e51ff
apexlions16 <hcywashere@gmail.com>
2026-08-07 16:19:03 +0300

    chore(player): bump version for quality selection

--- apps/android-player/app/build.gradle.kts
+++ apps/android-player/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.2.0"
 
         vectorDrawables {
             useSupportLibrary = true
