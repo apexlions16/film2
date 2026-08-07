fbafc46f95574c739db2bc77765900667ccafdd2
apexlions16 <hcywashere@gmail.com>
2026-08-07 18:08:19 +0300

    chore(player): bump fast-refresh build to 1.3.1

--- apps/android-player/app/build.gradle.kts
+++ apps/android-player/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.apexlions.film2.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.0"
+        versionCode = 8
+        versionName = "1.3.1"
 
         vectorDrawables {
             useSupportLibrary = true
